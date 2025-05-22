0fe448203bcffaa48796c10160223b150a15a7f7
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-23 00:39:02 +0330

    clean code prfile

--- core/src/main/java/com/tilldawn/view/ProfileMenuView.java
+++ core/src/main/java/com/tilldawn/view/ProfileMenuView.java
@@ -4,53 +4,53 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.tilldawn.Enum.Avatar;
 import com.tilldawn.Main;
-import com.tilldawn.control.LoginController;
 import com.tilldawn.control.MainMenuController;
 import com.tilldawn.control.ProfileController;
-import com.tilldawn.control.SignUpController;
-import com.tilldawn.model.GameAssetManager;
 import com.tilldawn.model.Result;
-import com.tilldawn.model.User;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.Timer;
-
 
 public class ProfileMenuView implements Screen {
     private Stage stage;
     private Skin skin;
     private TextField usernameField, passwordField;
-    private Image avatarImage;
     private FileHandle selectedAvatar;
 
     public ProfileMenuView(Skin skin, ProfileController controller) {
         this.skin = skin;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+
+        // Background
         Texture bgTexture = new Texture(Gdx.files.internal("backgrounds/1.png"));
         Image background = new Image(bgTexture);
         background.setFillParent(true);
         stage.addActor(background);
+
+        // Main Table
         Table table = new Table();
         table.setFillParent(true);
+        table.pad(30);
         stage.addActor(table);
+
+        // Message Table
         Table messageTable = new Table();
         messageTable.setFillParent(true);
         messageTable.left().pad(50);
         stage.addActor(messageTable);
         Label message = new Label("", skin);
+        messageTable.add(message).width(200).height(50);
+
         // Title
         table.add(new Label("Profile", skin, "title")).colspan(2).padBottom(30).row();
 
@@ -62,19 +62,9 @@ public class ProfileMenuView implements Screen {
             public void clicked(InputEvent event, float x, float y) {
                 Result result = controller.ChangeUsername(usernameField.getText());
                 message.setText(result.Message());
-                Timer.schedule(new Timer.Task() {
-                    @Override
-                    public void run() {
-                        Gdx.app.postRunnable(() -> {
-                            message.setText("");
-                        });
-                    }
-                }, 5);
+                scheduleMessageClear(message);
             }
-
         });
-        messageTable.add(message).width(100).height(50);
-
         table.add(new Label("Username", skin)).left();
         table.add(usernameField).width(300).row();
         table.add(changeUsernameBtn).colspan(2).padBottom(10).row();
@@ -89,24 +79,15 @@ public class ProfileMenuView implements Screen {
             public void clicked(InputEvent event, float x, float y) {
                 Result result = controller.changePassword(passwordField.getText());
                 message.setText(result.Message());
-                Timer.schedule(new Timer.Task() {
-                    @Override
-                    public void run() {
-                        Gdx.app.postRunnable(() -> {
-                            message.setText("");
-                        });
-                    }
-                }, 5);
+                scheduleMessageClear(message);
             }
         });
-
         table.add(new Label("Password", skin)).left();
         table.add(passwordField).width(300).row();
         table.add(changePassBtn).colspan(2).padBottom(10).row();
 
-        // Delete account
+        // Delete Account
         TextButton deleteBtn = new TextButton("Delete Account", skin);
-
         deleteBtn.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -116,62 +97,46 @@ public class ProfileMenuView implements Screen {
                             Result result = controller.DeleteAccount();
                             message.setText(result.Message());
                         }
-
-
                     }
-
                 };
-
                 confirmDialog.text("Are you sure you want to delete your account?");
                 confirmDialog.button("Yes", true);
                 confirmDialog.button("No", false);
                 confirmDialog.show(stage);
-                Timer.schedule(new Timer.Task() {
-                    @Override
-                    public void run() {
-                        Gdx.app.postRunnable(() -> {
-                            message.setText("");
-                        });
-                    }
-                }, 5);
+                scheduleMessageClear(message);
             }
         });
         table.add(deleteBtn).colspan(2).padBottom(20).row();
 
-        // Avatar section
-        Table avatarTable = new Table();
-
+        // Avatar Preview
         Image previewImage = new Image();
-        table.add(previewImage).colspan(2).left().row();
+        table.add(previewImage).colspan(2).left().padBottom(10).row();
 
+        // Avatar Selection
+        Table avatarTable = new Table();
         for (Avatar avatar : Avatar.values()) {
             Texture texture = new Texture(Gdx.files.internal(avatar.getPath()));
-
             ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
             style.imageUp = new TextureRegionDrawable(new TextureRegion(texture));
 
             ImageButton avatarBtn = new ImageButton(style);
-
             avatarBtn.addListener(new ClickListener() {
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
                     selectedAvatar = Gdx.files.internal(avatar.getPath());
-
                     previewImage.setDrawable(new TextureRegionDrawable(new TextureRegion(texture)));
                     Main.getApp().getCurrentUser().setAvatarPath(avatar.getPath());
                 }
             });
-
             avatarTable.add(avatarBtn).size(64).pad(10);
         }
 
         ScrollPane scrollPane = new ScrollPane(avatarTable, skin);
         scrollPane.setScrollingDisabled(false, true);
         scrollPane.setFadeScrollBars(false);
-
         table.add(scrollPane).colspan(2).height(100).width(Gdx.graphics.getWidth() * 0.8f).padBottom(20).row();
 
-
+        // Back Button
         TextButton backBtn = new TextButton("Back", skin);
         backBtn.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
@@ -181,11 +146,17 @@ public class ProfileMenuView implements Screen {
         table.add(backBtn).colspan(2);
     }
 
+    private void scheduleMessageClear(Label message) {
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                Gdx.app.postRunnable(() -> message.setText(""));
+            }
+        }, 5);
+    }
 
     @Override
-    public void show() {
-
-    }
+    public void show() {}
 
     @Override
     public void render(float delta) {
@@ -195,29 +166,17 @@ public class ProfileMenuView implements Screen {
     }
 
     @Override
-    public void resize(int i, int i1) {
-
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
-    public void dispose() {
-
-    }
-
-    // سایر متدهای Screen: resize, dispose ...
+    public void dispose() {}
 }
