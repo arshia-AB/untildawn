f1beb14def6669626576aaac926460438c7e51d6
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-23 23:05:29 +0330

    bug fixed

--- core/src/main/java/com/tilldawn/control/GameController.java
+++ core/src/main/java/com/tilldawn/control/GameController.java
@@ -1,18 +1,36 @@
 package com.tilldawn.control;
 
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.tilldawn.model.User;
+import com.tilldawn.model.Weapon;
 import com.tilldawn.view.GameView;
-
-import javax.swing.text.View;
 
 public class GameController {
     private GameView view;
+    private PlayerController playerController;
+    private WorldController worldController;
+    private WeaponController weaponController;
+
 
     public void setView(GameView view) {
-
         this.view = view;
+        playerController = new PlayerController(new User());
+        worldController = new WorldController(playerController);
+        weaponController = new WeaponController(new Weapon());
     }
 
+    public void updateGame() {
+        if (view != null) {
+            worldController.update();
+            playerController.update();
+            weaponController.update();
+        }
+    }
+
+    public PlayerController getPlayerController() {
+        return playerController;
+    }
+
+    public WeaponController getWeaponController() {
+        return weaponController;
+    }
 }
