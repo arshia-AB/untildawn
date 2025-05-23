6f55b0fcd99ec5c3fee581ca77c6fb63503092bf
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-23 21:06:45 +0330

    player controller zade shod

--- core/src/main/java/com/tilldawn/control/PlayerController.java
+++ core/src/main/java/com/tilldawn/control/PlayerController.java
@@ -1,4 +1,68 @@
-package com.tilldawn.control;
+package com.tilldawn.Control;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.tilldawn.Main;
+import com.tilldawn.model.GameAssetManager;
+import com.tilldawn.model.User;
 
 public class PlayerController {
+    private User player;
+
+    public PlayerController(User player){
+        this.player = player;
+    }
+
+    public void update(){
+        player.getPlayerSprite().draw(Main.getBatch());
+
+        if(player.isPlayerIdle()){
+            idleAnimation();
+        }
+
+        handlePlayerInput();
+    }
+
+
+    public void handlePlayerInput(){
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            player.setPosY(player.getPosY() - player.getSpeed());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            player.setPosX(player.getPosX() - player.getSpeed());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            player.setPosY(player.getPosY() + player.getSpeed());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            player.setPosX(player.getPosX() + player.getSpeed());
+            player.getPlayerSprite().flip(true, false);
+        }
+    }
+
+
+    public void idleAnimation(){
+        Animation<Texture> animation = GameAssetManager.getGameAssetManager().getCharacter1_idle_animation();
+
+        player.getPlayerSprite().setRegion(animation.getKeyFrame(player.getTime()));
+
+        if (!animation.isAnimationFinished(player.getTime())) {
+            player.setTime(player.getTime() + Gdx.graphics.getDeltaTime());
+        }
+        else {
+            player.setTime(0);
+        }
+
+        animation.setPlayMode(Animation.PlayMode.LOOP);
+    }
+
+    public User getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(User player) {
+        this.player = player;
+    }
 }
