2a73f176679793b7c4a4d4bdd95d2557d9357f32
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-23 23:03:03 +0330

    hamechi add shod

---
+++ core/src/main/java/com/tilldawn/control/WorldController.java
@@ -0,0 +1,23 @@
+package com.tilldawn.control;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.tilldawn.Main;
+
+public class WorldController {
+    private PlayerController playerController;
+    private Texture backgroundTexture;
+    private float backgroundX = 0;
+    private float backgroundY = 0;
+
+    public WorldController(PlayerController playerController) {
+        this.backgroundTexture = new Texture("GameBackground.png");
+        this.playerController = playerController;
+    }
+
+    public void update() {
+        backgroundX = playerController.getPlayer().getPosX();
+        backgroundY = playerController.getPlayer().getPosY();
+        Main.getBatch().draw(backgroundTexture, backgroundX, backgroundY);
+    }
+
+}
