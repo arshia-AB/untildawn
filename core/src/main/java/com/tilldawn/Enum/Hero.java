ffe6d3cec3380b09223d014b593876a4f3f95795
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-23 15:04:40 +0330

    hero path added

--- core/src/main/java/com/tilldawn/Enum/Hero.java
+++ core/src/main/java/com/tilldawn/Enum/Hero.java
@@ -1,16 +1,18 @@
 package com.tilldawn.Enum;
 
 public enum Hero {
-    SHANA("SHANA", 4, 4),
-    DIAMOND("DIAMOND", 7, 1),
-    SCARLET("SCARLET", 3, 5),
-    LILITH("LILITH", 5, 3),
-    DASHER("DASHER", 2, 10);
+    SHANA("heros/T_Shana_Portrait.png", "SHANA", 4, 4),
+    DIAMOND("heros/T_Diamond_Portrait.png", "DIAMOND", 7, 1),
+    SCARLET("heros/T_Scarlett_Portrait.png", "SCARLET", 3, 5),
+    LILITH("heros/T_Lilith_Portrait.png", "LILITH", 5, 3),
+    DASHER("heros/T_Dasher_Portrait.png", "DASHER", 2, 10);
+    private String path;
     private String Name;
     private int HP;
     private int Speed;
 
-    Hero(String Name, int Hp, int Speed) {
+    Hero(String path, String Name, int Hp, int Speed) {
+        this.path = path;
         this.Name = Name;
         this.HP = Hp;
         this.Speed = Speed;
