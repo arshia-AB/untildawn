670187e0ade4c2239837bca04647e5b5df78a781
arshix <93470822+arshixAB@users.noreply.github.com>
2025-05-25 10:18:12 +0330

    class enemy sakhte shod

--- core/src/main/java/com/tilldawn/model/Enemy.java
+++ core/src/main/java/com/tilldawn/model/Enemy.java
@@ -1,5 +1,7 @@
 package com.tilldawn.model;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class Enemy {
@@ -9,21 +11,36 @@ public abstract class Enemy {
     protected float speed;
     protected boolean alive = true;
     protected CollisionRect rect;
+    protected Sprite sprite;
     protected static final int SIZE = 32;
 
-    public Enemy(Vector2 spawnPos, int hp, float speed) {
+    public Enemy(Vector2 spawnPos, int hp, float speed, Texture texture) {
         this.position = spawnPos;
         this.hp = hp;
         this.speed = speed;
         this.velocity = new Vector2();
         this.rect = new CollisionRect(position.x, position.y, SIZE, SIZE);
+
+        this.sprite = new Sprite(texture);
+        this.sprite.setSize(SIZE, SIZE);
+        this.sprite.setPosition(position.x, position.y);
     }
 
     public void update(float delta, Vector2 playerPos) {
+        if (!alive) return;
+
         Vector2 direction = new Vector2(playerPos).sub(position).nor();
         velocity.set(direction.scl(speed));
         position.add(velocity.x * delta, velocity.y * delta);
+
         rect.move(position.x, position.y);
+        sprite.setPosition(position.x, position.y);
+    }
+
+    public void render(com.badlogic.gdx.graphics.g2d.SpriteBatch batch) {
+        if (alive) {
+            sprite.draw(batch);
+        }
     }
 
     public void takeDamage(int damage) {
--- core/src/main/java/com/tilldawn/model/TentacleMonster.java
+++ core/src/main/java/com/tilldawn/model/TentacleMonster.java
@@ -1,11 +1,13 @@
 package com.tilldawn.model;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 public class TentacleMonster extends Enemy {
+    private static final Texture texture = new Texture("enemy/T_TentacleEnemy_0.png");
 
     public TentacleMonster(Vector2 spawnPos) {
-        super(spawnPos, 25, 100); // HP: 25, speed: 100
+        super(spawnPos, 25, 100, texture);
     }
 
     @Override
